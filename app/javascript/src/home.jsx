45e67bf329c95d6287df276a7839ff501de26760
Nathan Peters <peters.nate8@gmail.com>
2024-05-29 16:20:22 -0600

    add layout component for home page and inject properties data

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,14 +1,68 @@
 // home.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Layout from '@src/layout';
+import { handleErrors } from '@utils/fetchHelper';
+import './home.scss'
 
-const Home = () => (
-  <h1>Home page</h1>
-)
+class Home extends React.Component {
+  state = {
+    properties: [],
+    total_pages: null,
+    next_page: null,
+    loading: true,
+  };
+
+  componentDidMount() {
+    fetch('/api/properties?page=1')
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          properties: data.properties,
+          total_pages: data.total_pages,
+          next_page: data.next_page,
+          loading: false,
+        });
+      })
+  }
+
+  render() {
+    const { properties, next_page, loading } = this.state;
+    return (
+      <Layout>
+        <div className='container pt-4'>
+          <h4 className='mb-1'>Top-rated places to stay</h4>
+          <p className='text-secondary mb-3'>Explore some of the best-reviewed stays in the world</p>
+          <div className='row'>
+            {properties.map((property) => {
+              return (
+                <div className='col-6 col-lg-4 mb-4 property' key={property.id}>
+                  <a href={`/property/${property.id}`} className='text-body text-decoration-none'>
+                    <div
+                      className='property-image mb-1 rounded'
+                      style={{ backgroundImage: `url(${property.image_url})` }}
+                    />
+                    <p className='text-uppercase mb-0 text-secondary'>
+                      <small>
+                        <b>{property.city}</b>
+                      </small>
+                    </p>
+                    <h6 className='mb-0'>{property.title}</h6>
+                    <p className='mb-0'>
+                      <small>${property.price_per_night} USD/night</small>
+                    </p>
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+          {loading && <p className='text-center'>loading...</p>}
+        </div>
+      </Layout>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Home />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+  ReactDOM.render(<Home />, document.body.appendChild(document.createElement('div')));
+});
