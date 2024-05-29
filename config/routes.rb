74b023ac404f59ba0aa44707d64d5f2fdb71c8ec
Nathan Peters <peters.nate8@gmail.com>
2024-05-29 14:02:19 -0600

    add properties migration, controller, model and views files

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     # Add routes below this line
     resources :users, only: [:create]
     resources :sessions, only: %i[create destroy]
+    resources :properties, only: [:index, :show]
 
     get '/authenticated' => 'sessions#authenticated'
   end
